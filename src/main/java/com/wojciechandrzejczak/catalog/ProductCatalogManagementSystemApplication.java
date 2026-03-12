fc6acc2ba0e647cef5723c362e24cd8bfebd6af6
Buumty <wojtekand22@gmail.com>
2026-03-12 19:35:38 +0100

    Adding Spring Initializr Project

---
+++ src/main/java/com/wojciechandrzejczak/catalog/ProductCatalogManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wojciechandrzejczak.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogManagementSystemApplication.class, args);
+	}
+
+}
